aef9f26dfda00514a8cf5f6b0725a3f7e08440e4
jun9ho <wjdgh9806@korea.ac.kr>
2025-08-28 16:29:09 +0900

    java-study: ch15

---
+++ java/ch15/Practice_1_1/Wrapping.java
@@ -0,0 +1,39 @@
+package ch15.Practice_1_1;
+
+
+class Box {
+    public void wrap() { 
+        System.out.println("Simple Wrapping");
+    }
+}
+
+class PaperBox extends Box {
+    @Override
+    public void wrap() {
+        System.out.println("Paper Wrapping");
+    }
+}
+
+class GoldPaperBox extends PaperBox {
+
+    @Override
+    public void wrap() {
+        System.out.println("Gold Wrapping");
+    }
+}
+
+class Wrapping {
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        PaperBox box2 = new PaperBox();
+        GoldPaperBox box3 = new GoldPaperBox();
+        
+        wrapBox(box1);
+        wrapBox(box2);
+        wrapBox(box3);
+    }
+
+    public static void wrapBox(Box box) {
+        box.wrap();
+    }
+}
